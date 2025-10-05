1328ed1e1e2799b086b9ff202662eb318b67e897
Daksh <dakshdaksh698@gmail.com>
2025-10-05 11:26:15 +0530

    Added function to Chunk the texts

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,File, UploadFile
+from worker import process_document
 
 
 app = FastAPI()
@@ -6,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Message" : "Check"}
+
+
+@app.post("/Documents/upload")
+async def upload_file(file: UploadFile = File(...)):
+    """Read contents and send to celery"""
+    contents = await file.read()
+    task = process_document.delay(contents)
+    return {"task_id": task.id, "status": "Processing started"}
+    
+    
--- backend/worker.py
+++ backend/worker.py
@@ -1,6 +1,7 @@
 from celery import Celery
 import os
 from dotenv import load_dotenv
+from pypdf import PdfReader
 
 load_dotenv()
 
@@ -10,3 +11,28 @@ celery_app = Celery(
     broker = os.getenv("CELERY_BROKER_URL"),
     backend = os.getenv("CELERY_RESULT_BACKEND")
 )
+
+def chunkText(text, chunkSize=1000, chunkOverlap=200):
+        step = chunkSize - chunkOverlap
+        chunks =[]
+        start = 0
+        while start < len(text):
+            end = start + chunkSize
+            chunk = text[start:end]
+            chunks.append(chunk)
+            start += step
+        return chunks
+
+
+
+
+@celery_app.task
+def process_document(file_contents):
+    try:
+        reader = PdfReader(file_contents)
+        text =""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+
+        chunks = chunkText(text, chunkSize=1000, chunkOverlap=200)
+        
