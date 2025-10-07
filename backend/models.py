55b9ce9927316fad91f0b65ed5ab4dcd8279924c
Daksh <dakshdaksh698@gmail.com>
2025-10-07 15:37:10 +0530

    Finished creating the model base for the table

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from pgvector.sqlalchemy import Vector 
+from sqlalchemy import Column,Integer,String,Text
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+class DocumentChunk(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    content: Mapped[str] = mapped_column(String, nullable=False)
+    embedding: Mapped[list[float]] = mapped_column(Vector(384))
+    
